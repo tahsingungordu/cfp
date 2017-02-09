6a3e200f282119d035af4d9ca6a54eca4bfabe8c
Tahsin Gungordu <tahsingungordu@gmail.com>
2017-02-10 02:10:46 +0300

    added choose skin in theme

--- template-parts/avatar.php
+++ template-parts/avatar.php
@@ -6,7 +6,7 @@
 
 <?php require_once get_template_directory() .'/template-parts/parts/header.php'; ?>
 
-<body id="image">
+<body id="image" <?php body_class(); ?>>
 	<?php $avatar_metadata = wp_get_attachment_metadata( $general_options['personal']['photo'] ); ?>
 	<img src="<?php echo wp_get_attachment_image_src( $general_options['personal']['photo'], 'full' )[0]; ?>" alt="<?php echo $general_options['personal']['name']; ?> Avatar">
 	<small>250 x 250 pixels (Natural: <?php echo $avatar_metadata['width'] ?> x <?php echo $avatar_metadata['height'] ?> pixels)</small>
